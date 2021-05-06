a5b7e69c598405d2c0035ffcc2d85974804b5cfd
Sofiane Nasri <sofiane.nasri.1995@gmail.com>
2021-05-06 11:17:08 +0200

    project initialization

---
+++ src/test/java/com/declaretonmorveux/declaretonmorveux/DeclaretonmorveuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.declaretonmorveux.declaretonmorveux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeclaretonmorveuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
