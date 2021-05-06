bdb18881cb18013f9c0cc8f8d245672bfb995279
Sofiane Nasri <sofiane.nasri.1995@gmail.com>
2021-05-06 15:36:37 +0200

    Allow new route to be access and add parsing + save in database

--- src/main/java/com/declaretonmorveux/declaretonmorveux/runner/DatasRunner.java
+++ src/main/java/com/declaretonmorveux/declaretonmorveux/runner/DatasRunner.java
@@ -1,6 +1,10 @@
 package com.declaretonmorveux.declaretonmorveux.runner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.declaretonmorveux.declaretonmorveux.feign.client.SchoolClient;
+import com.declaretonmorveux.declaretonmorveux.model.School;
 import com.declaretonmorveux.declaretonmorveux.service.SchoolService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -22,10 +26,22 @@ public class DatasRunner implements CommandLineRunner{
     public void run(String... args) throws Exception {
         String datas = schoolClient.getData();
         ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(datas).get("features");
 
-        JsonNode jsonNode = objectMapper.readTree(datas);
 
-        System.err.println(jsonNode.get("type"));
+        for(int i = 0; i < jsonNode.size(); i++){
+            String schoolName = jsonNode.get(i).get("properties").get("libel").toString();
+
+            if(!schoolName.isEmpty()){
+                School school = new School();
+                school.setName(schoolName);
+
+                schoolService.save(school);
+            }
+        }
+
+     
+      
     }
     
 }
