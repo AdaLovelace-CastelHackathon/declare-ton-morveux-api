56aef8973e96aed41e893f056d9b031eb6a7dab3
Sofiane Nasri <sofiane.nasri.1995@gmail.com>
2021-05-10 23:00:37 +0200

    Add a declarations controlller

--- src/main/java/com/declaretonmorveux/declaretonmorveux/service/DeclarationService.java
+++ src/main/java/com/declaretonmorveux/declaretonmorveux/service/DeclarationService.java
@@ -1,12 +1,16 @@
 package com.declaretonmorveux.declaretonmorveux.service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.declaretonmorveux.declaretonmorveux.model.Declaration;
 import com.declaretonmorveux.declaretonmorveux.repository.DeclarationRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import net.bytebuddy.asm.Advice.Local;
 
 @Service
 public class DeclarationService {
@@ -19,5 +23,12 @@ public class DeclarationService {
 
     public List<Declaration> getAll(){
         return declarationRepository.findAll();
+    }
+
+    public List<LocalDate> getAllBySchoolId(long schoolId){
+        List<Declaration> declarations = declarationRepository.getBySchoolId(schoolId);
+        List<LocalDate> dates = declarations.stream().map( e -> e.getDate()).collect(Collectors.toList());
+    
+        return dates;
     }
 }
